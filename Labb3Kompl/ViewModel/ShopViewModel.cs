0a1e7ed30dc9093268259a60d539910fac7ab670
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2021-12-29 14:58:19 +0100

    sort listview by product type done

--- Labb3Kompl/ViewModel/ShopViewModel.cs
+++ Labb3Kompl/ViewModel/ShopViewModel.cs
@@ -1,34 +1,40 @@
-﻿using Labb3Kompl.Managers;
+﻿using System;
+using Labb3Kompl.Managers;
 using Labb3Kompl.Model;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
 using MongoDB.Driver;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
+using MongoDB.Bson;
+using MongoDB.Driver.Core.Clusters.ServerSelectors;
 
 namespace Labb3Kompl.ViewModel
 {
     class ShopViewModel : ObservableObject
     {
-        public ObservableCollection<Produkt> Produkter { get; set; } = new();
+        public ObservableCollection<Produkt> Products { get; set; } = new();
+        public ObservableCollection<string> ProductType { get; set; } = new();
         public ObservableCollection<Produkt> UserCart { get; set; } = new();
         private readonly Managers.MongoDB _db = new("Butik");
         private NavigationManager _navigationManager;
         private UserManager _userManager = new();
-        private User _currentUser;
-        private Butik _currentStore;
         private Produkt _produkt;
         private IMongoDatabase _database;
-        private IMongoCollection<Produkt> _collection;
         public ICommand KundprofilViewCommand { get; }
         public ICommand AddToCartCommand => new RelayCommand(AddToCart);
 
         public ShopViewModel(NavigationManager navigationManager, UserManager userManager)
         {
             LoadProducts();
-            _userManager.CurrentUser.Kundkorg = UserCart; 
+            GetProductTypes();
+            LoadProductType();
+            _userManager.CurrentUser.Kundkorg = UserCart;
             _navigationManager = navigationManager;
             _userManager = userManager;
             KundprofilViewCommand = new RelayCommand(() => { _navigationManager.CurrentView = new KundProfilViewModel(_navigationManager, _userManager); });
@@ -40,6 +46,23 @@ namespace Labb3Kompl.ViewModel
         {
             get => _amount;
             set => SetProperty(ref _amount, value);
+        }
+
+        public ICollectionView ItemsView
+        {
+            get => CollectionViewSource.GetDefaultView(Products);
+        }
+
+        private string _selectedProductType;
+        public string SelectedProductType
+        {
+            get => _selectedProductType;
+            set
+            {
+                _selectedProductType = value;
+                OnPropertyChanged();
+                ItemsView.Refresh();
+            }
         }
 
         public Produkt SelectedProduct
@@ -91,8 +114,34 @@ namespace Labb3Kompl.ViewModel
 
             foreach (var item in collection)
             {
-                Produkter.Add(item);
+                Products.Add(item);
             }
+        }
+
+        public void GetProductTypes()
+        {
+            var dbClient = new MongoClient();
+            _database = dbClient.GetDatabase("Butik");
+            var collection = _database.GetCollection<Produkt>("Produkter");
+            
+            var productTypes = collection.AsQueryable().Select(e => e.ProductType).Distinct();
+
+            foreach (var items in productTypes)
+            {
+                ProductType.Add(items);
+            }
+        }
+
+        public void LoadProductType()
+        {
+            var source = CollectionViewSource.GetDefaultView(Products);
+            source.Filter = o => Filter(o as Produkt);
+        }
+
+        private bool Filter(Produkt type)
+        {
+            return SelectedProductType == null
+                || type.ProductType.IndexOf(SelectedProductType, StringComparison.OrdinalIgnoreCase) != -1;
         }
     }
 }
