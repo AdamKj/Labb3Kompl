bd9cd46f4856fa1b129658ad00539ac36051fb7b
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2021-12-29 12:20:44 +0100

    emptying customer cart issue solved

--- Labb3Kompl/Model/Butik.cs
+++ Labb3Kompl/Model/Butik.cs
@@ -11,8 +11,7 @@ namespace Labb3Kompl.Model
     class Butik
     {
         private UserManager _userManager = new();
-        private Produkt _produkt = new();
-        private IMongoDatabase _database;
+        private readonly Managers.MongoDB _db = new("Butik");
         public string Username { get; set; }
         public User Admin { get; set; }
 
@@ -26,24 +25,10 @@ namespace Labb3Kompl.Model
         public async Task CheckOutUser(User user)
         {
             _userManager.CurrentUser = user;
-            //user.Kundkorg.Clear();
-            ClearCart();
+            user.Kundkorg.Clear();
+            await _db.UpsertRecord("Users", _userManager.CurrentUser);
             MessageBox.Show("Tack för att du handlade i butiken!", "Goodbye", MessageBoxButton.OK);
             Application.Current.Shutdown();
-        }
-
-        public void ClearCart()
-        {
-            var dbClient = new MongoClient();
-            _database = dbClient.GetDatabase("Butik");
-            var collection = _database.GetCollection<User>("Users");
-
-            //var update = Builders<User>.Update.PullFilter(p => p.Kundkorg, f => f.ObjectId == _produkt.ObjectId);
-            //var result = collection.FindOneAndUpdateAsync(p => p.ObjectId == _userManager.CurrentUser.ObjectId, update)
-            //    .Result;
-
-            var update = Builders<User>.Update.PullFilter(x => x.Kundkorg, Builders<Produkt>.Filter.Eq(x => x.ObjectId, _produkt.ObjectId));
-            collection.FindOneAndUpdateAsync(x => x.ObjectId.Equals(_userManager.CurrentUser.ObjectId), update);
         }
     }
 }
